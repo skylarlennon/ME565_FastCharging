289b9e75a47568117b77334627245b69be1376d7
Skylar <skylarl@umich.edu>
2025-04-07 16:01:55 -0400

    Initial commit, psudo code

---
+++ models/battery_params.m
@@ -0,0 +1,20 @@
+%% MECHENG 565 Project: Fast Charging Group 1
+clc;clear;close all;
+%% ===============Contributors===============
+% Vijay Balasekaran     vbalasek@umich.edu
+% Clayton Garmon        cjgarmon@umich.edu
+% Skylar Lennon         skylarl@umich.edu
+% Justin Ryu            jusryu@umich.edu
+% Emma Tum Suden        emmadt@umich.edu
+
+%% =============Battery Parameters=============
+% - Include all of the necessary constants here
+
+%% =============Load Current Profiles=============
+% - Load the current profiles here
+
+%% =============Simulate=============
+% - Run the simulation here N = 1000 times
+
+%% =============Plot Results=============
+% - Parse the results and plot here
---
+++ models/generate_current_profiles.m
@@ -0,0 +1,29 @@
+%% MECHENG 565 Project: Fast Charging Group 1
+clc;clear;close all;
+%% ===============Contributors===============
+% Vijay Balasekaran     vbalasek@umich.edu
+% Clayton Garmon        cjgarmon@umich.edu
+% Skylar Lennon         skylarl@umich.edu
+% Justin Ryu            jusryu@umich.edu
+% Emma Tum Suden        emmadt@umich.edu
+
+%% ===============Parameters===============
+% - Add constants here
+% - Include constraints
+%   - Max battery charging/discharging power
+%   - Max temperature (would need to integrate with the battery SOH_&_Temp
+%   model?
+% - Perhaps need to load battery parameters, or run the battery simulation
+% - Include things like delta SOC & goal charging time, tolerances, etc. 
+
+% - Run the simulation and the optimizer to generate the charging profile
+
+% - Save the charging profile(s) to be used in the battery degredation
+% simulations
+
+
+% Notes:
+% - Perhaps to simplify, we only include effects of charging and not
+% discharging, don't really know how to estimate the expected discharge
+% profiles of an EV unless we wanted to run like a EPA drive cycle and
+% simple road load model
